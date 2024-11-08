7113d95cf88eb9321f5bb3b163f123805bedd1a6
sujoy-dam <sujooy01@gmail.com>
2024-11-09 01:03:07 +0600

    added details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import Heading from "./Heading"
+import Img from "../assets/nutrition.png"
 
 const Details = () => {
-    const {id}=useParams()
+    const { id } = useParams()
     const data = useLoaderData();
-    console.log(data)
-    const singleData = data.find(coffee=>coffee.id == id)
-const {name}=singleData
-    // console.log(typeof id,singleData)
+    const [coffee, setCoffee] = useState({})
+    // console.log(data)
+    useEffect(() => {
+
+        const singleData = data.find(coffee => coffee.id == id)
+        setCoffee(singleData)
+    }, [data, id])
+    const { name, image, category, ingredients, description, popularity, origin, rating, making_process, nutrition_info } = coffee
+    // const key = Object.keys(nutrition_info)
+    // console.log(key)
     return (
         <div>
-            <h1>Details{name}</h1>
+            <h1 className='text-4xl'>{description}</h1>
+            <img className='w-full' src={image} alt="" />
+            <div className='mb-5'>
+                <h1 className='text-3xl'>{origin} Coffee</h1>
+                <p>Popularity: {popularity}</p>
+                <p>Rating: {rating}</p>
+            </div>
+            <div className='mb-5'>
+                <h1 className='text-3xl'>Making Process</h1>
+                <p>{making_process}</p>
+            </div>
+            <div className='flex justify-between gap-32'>
+                <div>
+                    <div className='mb-5'>
+                        <p className='text-3xl'>Ingredients:</p>
+                        {
+                            // ingredients.map(ingredient=>console.log(ingredient))
+                        }
+
+                    </div>
+                    <div className='mb-5'>
+                        <p className='text-3xl'>Nutrition:</p>
+
+                    </div>
+                </div>
+                <div>
+                        <img src={Img} alt="" />
+                </div>
+            </div>
+
         </div>
     );
 };
